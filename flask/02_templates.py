f7741c33126e104a9989e845086d284b36ea9b0e
Chaddaï Fouché <chaddai.fouche@gmail.com>
2017-04-17 15:21:36 +0200

    Progress on Flask

--- flask/02_templates.py
+++ flask/02_templates.py
@@ -1,11 +1,26 @@
 ﻿# N'oubliez pas d'importer render_template
 from flask import Flask, render_template
+from random import randint
 app = Flask("Templates")
-
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    # usage de base
+    return render_template("helloworld.html")
 
+@app.route("/test")
+def test():
+    # on peut passer n'importe quelles valeurs au template de façon à ce qu'il puisse
+    # les afficher et les explorer. Le nom dans le programme Python n'importe pas,
+    # seulement le nom du paramètre dans l'appel à render_template()
+    var = "truc"
+    return render_template("test.html", liste=[1,2,3], machin=var)
+
+# Mais l'usage classique consiste à construire des valeurs à partir du chemin ou
+# de vos fonctions Python et de les passer sous le même nom au template
+@app.route("/passage/<name>/<int:num>")
+def passage(name, num):
+    loops = randint(1,4)
+    return render_template("passage.html", name=name, num=num, loops=loops)
 
 app.run(debug=True)
---
+++ flask/03_forms_and_requests.py
@@ -0,0 +1,20 @@
+﻿# N'oubliez pas d'importer request
+from flask import Flask, render_template, request
+from random import randint
+app = Flask("Forms and Request")
+
+@app.route("/")
+def form():
+    # servons un formulaire à la racine
+    return render_template("form.html")
+
+# l'action du formulaire doit mener à /resultat avec la méthode POST et il faut
+# indiquer cette méthode dans @app.route() (par défaut seule la méthode GET est
+# prise en charge).
+@app.route("/resultat", methods=['POST'])
+def resultat():
+    # l'objet request contient un attribut form qui est un dictionnaire des
+    # noms du formulaire vers les valeurs associées
+    return "L'utilisateur " + request.form['username'] + " a écrit le texte : " + request.form['texteComplet']
+
+app.run(debug=True)
